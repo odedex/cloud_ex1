995c0d546f59298e92f7e71db0a6b4bd8ef284d5
odedex <karitvruda@hotmail.com>
2016-03-06 14:12:26 +0200

    klhb

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.use('/upload', function (req, res, next){
+  // uses the upload library
+
+  res.render('index', { title: 'upload' });
+
+});
+
+router.use('/getfile/:fileid/', function (req, res, next){
+  // uses the upload library
+  // should use basic HTTP AUTH
+
+  res.render('index', { title: 'getfile' });
+
+});
+
+router.use('/sendmail/', function (req, res, next){
+  // uses the upload library
+  // bodyParams - subject, content, to
+
+  res.render('index', { title: 'sendmail' });
+
+});
+
+
+
 module.exports = router;
